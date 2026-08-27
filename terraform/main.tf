49deb41177973374eb9f4ec995ffd7fac4dbed76
Onkar Chavan <onkarvchavan2017@gmail.com>
2026-08-28 02:30:40 +0530

    feat: add managed node group with NAT egress and node IAM policies

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,11 +94,41 @@ resource "aws_route_table_association" "public_b" {
 }
 
 # -------------------------
+# NAT Gateway
+# -------------------------
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_a.id
+
+  depends_on = [
+    aws_internet_gateway.main
+  ]
+
+  tags = {
+    Name = "${var.project_name}-nat-gw"
+  }
+}
+
+# -------------------------
 # Private routing
 # -------------------------
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
 
   tags = {
     Name = "${var.project_name}-private-rt"
@@ -116,7 +146,7 @@ resource "aws_route_table_association" "private_b" {
 }
 
 # -------------------------
-# IAM Role
+# EKS Cluster IAM Role
 # -------------------------
 
 resource "aws_iam_role" "eks_cluster" {
@@ -146,6 +176,82 @@ resource "aws_iam_role" "eks_cluster" {
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# -------------------------
+# EKS Node IAM Role
+# -------------------------
+
+resource "aws_iam_role" "eks_node" {
+  name = "${var.project_name}-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-eks-node-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_pull_only" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+}
+
+# -------------------------
+# EKS Managed Node Group
+# -------------------------
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.project_name}-node-group"
+  node_role_arn   = aws_iam_role.eks_node.arn
+
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id,
+  ]
+
+  instance_types = ["t3.small"]
+
+  scaling_config {
+    desired_size = 2
+    min_size     = 2
+    max_size     = 2
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node,
+    aws_iam_role_policy_attachment.ecr_pull_only,
+    aws_iam_role_policy_attachment.eks_cni,
+  ]
+
+  tags = {
+    Name = "${var.project_name}-node-group"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 # -------------------------
